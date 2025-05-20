453551d1923ef06caa6eddbed626966e70c1ea38
Apriyanto20 <adiapriyanto935@gmail.com>
2025-05-20 15:38:24 +0700

    Add Booking with API

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,23 +1,26 @@
-
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { BookingTables } from "@/components/Tables/bookings/bookingTables";
 
 import { Metadata } from "next";
+import { BookingAdd } from "../../components/Modal/booking/BookingAdd";
+import { BookingModalProvider } from "@/components/Modal/booking/BookingModalContex";
 
 export const metadata: Metadata = {
-    title: "Tables",
+  title: "Tables",
 };
 
 const TablesPage = () => {
-    return (
-        <>
-            <Breadcrumb pageName="Bookings" />
-
-            <div className="space-y-10">
-                <BookingTables />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <BookingModalProvider>
+        <Breadcrumb pageName="Bookings" />
+        <BookingAdd />
+        <div className="space-y-10">
+          <BookingTables />
+        </div>
+      </BookingModalProvider>
+    </>
+  );
 };
 
 export default TablesPage;
